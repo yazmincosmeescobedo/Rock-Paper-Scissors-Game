c2535d4c6997273d2434df2a2e9da55c63d84be3
yazmincosmeescobedo <yazmincosmeescobedo@gmail.com>
2022-07-14 23:28:57 -0400

    commit

---
+++ app.js
@@ -0,0 +1,33 @@
+function rpsgame(){
+
+
+    /* Global Vars and Constants */
+    var playerscore = 0;
+    var kaijiscore = 0; 
+    var matches = 0; 
+    const rockbutton = document.querySelector('.rock');
+    const paperbutton = document.querySelector('.paper');
+    const scissorbutton = document.querySelector('.scissor');
+    const rpsoptions = [rockBtn,paperBtn,scissorBtn];
+    const computerOptions = ['rock','paper','scissors'];
+
+
+
+    function rungame(){
+
+
+
+    }
+
+    function compareoptions(){
+
+    }
+
+
+
+
+
+
+
+
+}
